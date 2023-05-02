ce1ce0ebdb63b8931c93c86ef7a7e542129f0f40
JonathanTaglioneCS <107624147+JonathanTaglioneCS@users.noreply.github.com>
2023-05-02 08:49:26 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ from distutils.core import setup
 scripts = glob.glob('bin/*')
 
 setup(
-  name='docx2txt',
-  packages=['docx2txt'],
+  name='docx2txtextended',
+  packages=['docx2txtextended'],
   version='0.8',
   description='A pure python-based utility to extract text and images '
               'from docx files.',
-  author='Ankush Shah',
-  author_email='ankush.shah.nitk@gmail.com',
-  url='https://github.com/ankushshah89/python-docx2txt',
-  download_url='https://github.com/ankushshah89/python-docx2txt/tarball/0.8',
+  author='J Taglione',
+  author_email='',
+  url='https://github.com/JonathanTaglioneCS/python-docx2txtextended',
+  download_url='',
   keywords=['python', 'docx', 'text', 'images', 'extract'],
   scripts=scripts,
   classifiers=[],
